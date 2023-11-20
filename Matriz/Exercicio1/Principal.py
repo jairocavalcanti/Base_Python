98f5b2eb212bf3a811808ae4ef263a569bd963c4
Jairo <jairoregiscavalcanti@gmail.com>
2023-11-20 19:43:41 -0300

    Exercicio 1 inserido

---
+++ Matriz/Exercicio1/Principal.py
@@ -0,0 +1,33 @@
+
+
+matriz = []
+
+linhas = int(input("Insira quantos alunos tera a matriz: "))
+coluna = 4
+
+contador = 0
+contador_de_notas = 0
+
+for i in range(linhas):
+    linha = []
+    for j in range(coluna):
+        contador = i+1
+        notas = float(input(f"insira as notas do aluno {contador}  : [{i}] [{j}] :"))
+        linha.append(notas)
+        contador_de_notas += notas
+       
+    matriz.append(linha)
+   
+print()  
+print("----------------------------------")    
+print("-- MATRIZ -- ")  
+cont = 0
+
+for linha in matriz:
+  cont += 1
+  print(f"Notas do aluno {cont} :" ,linha)
+
+for i in range(len(matriz)):
+    print(matriz[i][j])
+    
+print(contador_de_notas)
